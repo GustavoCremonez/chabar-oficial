4e94bf5cbf55dfad43e8446737817fbe7e47ad54
Gustavo Cremonez <gustavocremonezgc@gmail.com>
2024-10-24 18:24:17 -0300

    chore: adiciona animações ao layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   HostListener,
   inject,
   OnDestroy,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from 'gsap';
+import { ScrollTrigger, TextPlugin } from 'gsap/all';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, interval, Subject, Subscription, takeUntil } from 'rxjs';
 import { CheckinModalComponent } from './checkin-modal/checkin-modal.component';
 import { SupabaseService } from './core/supabase.service';
+
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
 interface GeoMarker {
   lat: number;
@@ -31,11 +38,14 @@ interface Countdown {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   @HostListener('window:resize')
   onResize() {
     this.updateMapSize();
   }
+  @ViewChild('heartSVG', { static: true }) heartSVG!: ElementRef;
+  @ViewChild('suggestionsContainer') suggestionsContainer!: ElementRef;
+  @ViewChild('receivedContainer') receivedContainer!: ElementRef;
 
   private subscription?: Subscription;
   private readonly _destroyed$: Subject<void> = new Subject<void>();
@@ -68,6 +78,56 @@ export class AppComponent implements OnInit, OnDestroy {
 
     this.updateMapSize();
     this.getGiftList();
+    this.animateHeart();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.setupScrollTrigger();
+    }, 0);
+  }
+
+  setupScrollTrigger() {
+    const suggestionsItems =
+      this.suggestionsContainer.nativeElement.querySelectorAll('li');
+    const receivedItems =
+      this.receivedContainer.nativeElement.querySelectorAll('li');
+
+    const allItems = [...suggestionsItems, ...receivedItems];
+
+    gsap.set(allItems, { opacity: 0, x: -50 });
+
+    ScrollTrigger.create({
+      trigger: this.suggestionsContainer.nativeElement,
+      start: 'top bottom-=100',
+      end: 'bottom top+=100',
+      onEnter: () => {
+        this.animateItems(suggestionsItems, true);
+      },
+      toggleActions: 'play none none reverse',
+    });
+
+    ScrollTrigger.create({
+      trigger: this.receivedContainer.nativeElement,
+      start: 'top bottom-=100',
+      end: 'bottom top+=100',
+      onEnter: () => {
+        this.animateItems(receivedItems, true);
+      },
+      toggleActions: 'play none none reverse',
+    });
+  }
+
+  private animateItems(items: Element[], show: boolean) {
+    gsap.to(items, {
+      opacity: show ? 1 : 0,
+      x: show ? 0 : -50,
+      duration: 0.5,
+      scrub: true,
+      stagger: 0.1,
+      ease: show ? 'power2.out' : 'power2.in',
+      delay: 0.02,
+    });
   }
 
   openCheckinModal() {
@@ -87,6 +147,16 @@ export class AppComponent implements OnInit, OnDestroy {
       });
   }
 
+  animateHeart() {
+    gsap.to(this.heartSVG.nativeElement, {
+      scale: 1.2,
+      duration: 0.5,
+      repeat: -1,
+      yoyo: true,
+      ease: 'power1.inOut',
+    });
+  }
+
   private async getGiftList(): Promise<void> {
     const receivedGifts = await this._supabaseService.getAllGifts(true);
     this.alreadyReceivedGifts = new Set(
@@ -99,6 +169,7 @@ export class AppComponent implements OnInit, OnDestroy {
     );
 
     this.syncRealtimeGiftsChanges();
+    setTimeout(() => this.setupScrollTrigger(), 0);
   }
 
   private async syncRealtimeGiftsChanges(): Promise<void> {
